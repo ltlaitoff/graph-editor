047bbdb156dda1e3532a3ce2405f8c8c9b4f6a95
ltlaitoff <ltlaitoff@gmail.com>
2023-10-19 13:56:21 +0300

    refactor: Format all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'university-coursework-1-OOP';
+	title = 'university-coursework-1-OOP'
 }
